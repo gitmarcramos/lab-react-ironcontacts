0321d8b70f2b2c39e64cc5777312efe95cefa8b4
Marc Ramos <marcramos.webdev@gmail.com>
2021-11-02 17:50:24 +0100

    wiiiip

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import "./App.css";
+import React from "react";
+import contact from "./contacts.json";
+const fiveContact = contact.slice(0, 5);
+
+class App extends React.Component {
+  state = {
+    contact: fiveContact,
+  };
+
+  /* -------------------------- // Add random contact ------------------------- */
+  addRandomContact() {
+    this.setState({
+      contact: [
+        ...this.state.contact,
+        contact[Math.floor(Math.random() * contact.length)],
+      ],
+    });
+
+    /* -- ===> ca fonctionne mais la solution du haut est visiblement mieux... -- */
+    // const actualState = [...this.state.contact];
+    // actualState.push(contact[Math.floor(Math.random() * contact.length)]);
+  }
+
+  /* ----------------------------- // Sort by name ---------------------------- */
+  sortByName() {
+    const actualState = [...this.state.contact];
+    actualState.sort((a, b) => {
+      return a.name > b.name;
+    });
+    this.setState({ contact: actualState });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={(e) => this.addRandomContact()}>
+          Add random contact
+        </button>
+        <button onClick={(e) => this.sortByName()}>Sort by name</button>
+
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.contact.map((contact, index) => {
+              return (
+                <tr key={index}>
+                  <td>
+                    <img src={contact.pictureUrl} alt="some text" />
+                  </td>
+                  <td>{contact.name}</td>
+                  <td>{contact.popularity}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default App;
